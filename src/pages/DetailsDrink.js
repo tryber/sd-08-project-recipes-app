9c027fa034362e37ddc733202dd3731ab658a284
Bruno Wedley <brunowedley27@gmail.com>
2021-03-23 18:31:40 -0300

    adiciona req 33 incompleto, falta a verificacao api de bebidas

--- src/pages/DetailsDrink.js
+++ src/pages/DetailsDrink.js
@@ -1,6 +1,9 @@
 import React, { useEffect, useContext, useState } from 'react';
 import { useParams } from 'react-router-dom';
 import { DataDrinksContext } from '../context/ContextDrinks';
+import { getDrinksDetails } from '../services/getAPIs';
+import shareIcon from '../images/shareIcon.svg';
+import whiteHeartIcon from '../images/whiteHeartIcon.svg';
 import './DetailsDrink.css';
 
 function DetailsDrink() {
@@ -10,18 +13,68 @@ function DetailsDrink() {
   const [drinkDetail, setDrinkDetail] = useState([]);
   useEffect(() => {
     const result = drinks.filter((drink) => drink.idDrink === Params.id);
-    setDrinkDetail(result);
+    const resultsMap = result.map((drink) => drink.idDrink);
+    async function fetchDetails() {
+      const saveDetail = await getDrinksDetails(resultsMap);
+      setDrinkDetail(saveDetail);
+      console.log(saveDetail);
+    }
+    fetchDetails();
   }, [drinks, Params.id]);
+
+  console.log(drinkDetail);
+
   return (
     <div>
       <div className="container-card-drink-details">
-        {drinkDetail.map((drink) => (
-          <div className="card-drink-details" key={ drink.idDrink }>
-            <img src={ drink.strDrinkThumb } alt="thumbnails-drink" />
-            <h2>{drink.strDrink}</h2>
+        {drinkDetail.map((drink, index) => (
+          <div
+            className="card-drink-details"
+            key={ drink.idDrink }
+          >
+            <img
+              data-testid="recipe-photo"
+              src={ drink.strDrinkThumb }
+              alt="thumbnails-drink"
+            />
+            <h2 data-testid="recipe-title">{drink.strDrink}</h2>
+            <p data-testid="recipe-category">{drink.strCategory}</p>
+            <h3>Ingredients</h3>
+            <ul>
+              <li data-testid={ `${index}-ingredient-name-and-measure` }>
+                {drink.strIngredient}
+              </li>
+            </ul>
+            <h4>Instructions</h4>
+            <p data-testid="instructions">{drink.strInstructions}</p>
+            <h4>Video</h4>
+            <video data-testid="video" src={ drink.strYoutube }>
+              <track
+                default
+                kind="captions"
+                srcLang="pt-br"
+                src={ drink.strYoutube }
+              />
+            </video>
+            <h4>Recomendadas</h4>
+            <img
+              data-testid={ `${index}-recomendation-card` }
+              src=""
+              alt="recomendations"
+            />
+            <button data-testid="start-recipe-btn" type="button">
+              Iniciar Receita
+            </button>
+
           </div>
         ))}
       </div>
+      <button type="button">
+        <img data-testid="share-btn" src={ shareIcon } alt="share-icon" />
+      </button>
+      <button type="button">
+        <img data-testid="favorite-btn" src={ whiteHeartIcon } alt="favorite-icon" />
+      </button>
     </div>
   );
 }
--- src/pages/DetailsMeal.js
+++ src/pages/DetailsMeal.js
@@ -1,6 +1,10 @@
 import React, { useEffect, useContext, useState } from 'react';
 import { useParams } from 'react-router-dom';
 import { LoginAndFoodContext } from '../context/ContextFood';
+import { getMealsDetails } from '../services/getAPIs';
+import shareIcon from '../images/shareIcon.svg';
+import whiteHeartIcon from '../images/whiteHeartIcon.svg';
+// import blackHeartIcon from '../images/blackHeartIcon.svg';
 import './DetailsMeal.css';
 
 function DetailsMeal() {
@@ -10,18 +14,65 @@ function DetailsMeal() {
   const [mealDetail, setMealDetail] = useState([]);
   useEffect(() => {
     const result = meals.filter((meal) => meal.idMeal === Params.id);
-    setMealDetail(result);
+    const resultsMap = result.map((meal) => meal.idMeal);
+    async function fetchDetails() {
+      const saveDetail = await getMealsDetails(resultsMap);
+      setMealDetail(saveDetail);
+    }
+    fetchDetails();
   }, [meals, Params.id]);
+
+  console.log(mealDetail);
+
   return (
     <div>
       <div className="container-card-meal-details">
-        {mealDetail.map((meal) => (
+        {mealDetail.map((meal, index) => (
           <div className="card-meal-details" key={ meal.idMeal }>
-            <img src={ meal.strMealThumb } alt="thumbnails-meal" />
-            <h2>{meal.strMeal}</h2>
+            <img
+              data-testid="recipe-photo"
+              src={ meal.strMealThumb }
+              alt="thumbnails-meal"
+            />
+            <h2 data-testid="recipe-title">{meal.strMeal}</h2>
+            <p data-testid="recipe-category">{meal.strCategory}</p>
+            <h3>Ingredients</h3>
+            <ul>
+              <li data-testid={ `${index}-ingredient-name-and-measure` }>
+                {meal.strIngredient}
+              </li>
+            </ul>
+            <h4>Instructions</h4>
+            <p data-testid="instructions">{meal.strInstructions}</p>
+            <h4>Video</h4>
+            <video data-testid="video" src={ meal.strYoutube }>
+              <track
+                default
+                kind="captions"
+                srcLang="pt-br"
+                src={ meal.strYoutube }
+              />
+            </video>
+            <h4>Recomendadas</h4>
+            <img
+              data-testid={ `${index}-recomendation-card` }
+              src=""
+              alt="recomendations"
+            />
+            <button data-testid="start-recipe-btn" type="button">
+              Iniciar Receita
+            </button>
+
           </div>
         ))}
       </div>
+      <button type="button">
+        <img data-testid="share-btn" src={ shareIcon } alt="share-icon" />
+      </button>
+      <button type="button">
+        <img data-testid="favorite-btn" src={ whiteHeartIcon } alt="favorite-icon" />
+      </button>
+
     </div>
   );
 }
